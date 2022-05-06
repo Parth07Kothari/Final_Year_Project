1305ede57e08215431c4c0d53bcbc1483e734dd9
Parth Kothari <parthmkothari@gmail.com>
2022-05-06 23:12:16 +0530

    first commit

---
+++ client/src/components/studentComponent/StudentMenu.js
@@ -0,0 +1,32 @@
+import {
+  HomeOutlined,
+  PictureOutlined,
+} from '@ant-design/icons';
+import { useQuery } from '@apollo/react-hooks';
+import { Menu } from 'antd';
+import React, { useContext, useEffect } from 'react';
+// import { FpsView } from 'react-fps';
+import { Link } from 'react-router-dom';
+import { EnrolmentContext, NavbarContext } from '../../context';
+import { CheckError } from '../../utils/ErrorHandling';
+
+export default () => {
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
+
+  const { enrolCount, getEnrolCount } = useContext(EnrolmentContext);
+
+  const { collapsed } = useContext(NavbarContext);
+
+  return (
+    <Menu theme='dark' mode='vertical' defaultSelectedKeys={[path]}>
+      <Menu.Item key={'dashboard'} icon={<HomeOutlined />}>
+        <Link to={'/dashboard'}>Courses</Link>
+      </Menu.Item>
+      <Menu.Item key={'facegallery'} icon={<PictureOutlined />}>
+        <Link to={'/facegallery'}>Face Gallery</Link>
+      </Menu.Item>
+      {/* {!collapsed && <FpsView width={190} height={100} top={525} />} */}
+    </Menu>
+  );
+};
---
+++ client/src/pages/common/mainMenu/MainMenu.js
@@ -0,0 +1,49 @@
+import { Button, Col, Divider, Layout, Row, Typography } from 'antd';
+import Texty from 'rc-texty';
+import 'rc-texty/assets/index.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HeaderNavbar } from '../../../components/common/mainMenu';
+import { Footer } from '../../../components/common/sharedLayout';
+
+const { Content } = Layout;
+const { Title } = Typography;
+
+export default () => {
+  return (
+    <Layout className='home layout'>
+      <HeaderNavbar />
+      <Content>
+        <Row align="middle">
+          <Col style={{ top: '50px' }} span={24} align="middle">
+            <Title level={3}>
+              <Texty
+                type="left"
+                mode="random"
+              >
+                Welcome to Attendance System
+              </Texty>
+            </Title>
+            <Texty
+              type="left"
+              mode="smooth"
+              interval="20"
+            >
+              An attendance tracking app with facial recognition
+            </Texty>
+
+            <br />
+            <Button type='primary' htmlType='submit'>
+              <Link to='/signup'>Get Started</Link>
+            </Button>
+            <Divider />
+            <div>
+              First time user? Please visit <Link to='/userguidelines'> User Guidelines</Link>
+            </div>
+          </Col>
+        </Row>
+      </Content>
+      <Footer />
+    </Layout>
+  );
+};
